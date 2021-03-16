3841dfbf58be8cfa8d6c008ce462075ef0689180
Dan Covic <dan.covic@outlook.com>
2021-03-16 20:55:05 +0100

    first commit

---
+++ servicenow-scripts.js
@@ -0,0 +1,6 @@
+var target = new GlideRecord('incident'); 
+target.addQuery('priority',1);
+target.query(); // Issue the query to the database to get relevant records 
+while (target.next()) { 
+  // add code here to process the incident record 
+}
