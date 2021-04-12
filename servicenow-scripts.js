365668686dd0d89033eff679c02346aa1ddcf29d
Dan Covic <dan.covic@outlook.com>
2021-04-12 14:00:12 +0200

    update field

--- servicenow-scripts.js
+++ servicenow-scripts.js
@@ -1,6 +1,16 @@
+// example
 var target = new GlideRecord('incident'); 
 target.addQuery('priority',1);
 target.query(); // Issue the query to the database to get relevant records 
 while (target.next()) { 
   // add code here to process the incident record 
+}
+
+// update INC/problem/change request
+var gr = new GlideRecord('change_task');
+	gr.addQuery('number', 'CTASK0111702');
+	gr.query();
+	if(gr.next()) {
+           gr.state = '4';
+           gr.update();
 }
