39e44c844bfb3600d37bcf99b6b05ecf07f9a361
Dan Covic <dan.covic@outlook.com>
2021-04-14 23:55:56 +0200

    CREATE NEW CHANGE TASK
    Create new change tasks for multiple change requests

--- servicenow-scripts.js
+++ servicenow-scripts.js
@@ -6,11 +6,69 @@ while (target.next()) {
   // add code here to process the incident record 
 }
 
-// update INC/problem/change request
+// UPDATE CHANGE TASK
 var gr = new GlideRecord('change_task');
+gr.addQuery('number', 'CTASK0111702');
+gr.query();
+if(gr.next()) {
+   gr.state = '4';
+   gr.update();
+}
+
+// CANCEL CHANGE TASKS OF A CHANGE REQUEST
+var change_arr = ['CHG0055651', 'CHG0057994', 'CHG0059665','CHG0060561']; //Iterate through the changes that need their change tasks to be cancelled
+
+for (var i=0; i < change_arr.length; i++){
+	
+	// UPDATE CHANGE TASK
+	var gr = new GlideRecord('change_task');
 	gr.addQuery('number', 'CTASK0111702');
 	gr.query();
 	if(gr.next()) {
            gr.state = '4';
            gr.update();
+	}
+}
+
+
+// CREATE NEW CHANGE TASK
+var change_arr = ['CHG0040003', 'CHG0040002']; //Iterate through the changes that need these new change tasks
+
+for (var i=0; i < change_arr.length; i++){
+	// Create first change_task for each of the changes listed in the above array
+	var gr = new GlideRecord('change_task');
+	gr.change_request = getSysid(change_arr[i]);
+	gr.order = 100;
+	gr.short_description = 'Notify Service Desk';
+	gr.assignment_group = 'Change Management';
+	gr.insert();
+	
+	// Create 2nd change_task for each of the changes listed in the above array
+	var gr = new GlideRecord('change_task');
+	gr.change_request = getSysid(change_arr[i]);
+	gr.order = 200;
+	gr.short_description = 'Implement the change';
+	gr.assignment_group = 'Change Management';
+	gr.insert();
+	
+	// Create 3nd change_task for each of the changes listed in the above array
+	var gr = new GlideRecord('change_task');
+	gr.change_request = getSysid(change_arr[i]);
+	gr.order = 300;
+	gr.short_description = 'Validate the change';
+	gr.assignment_group = 'Change Management';
+	gr.insert();
+}
+
+function getSysid(num){
+
+	// get the sys_id for a change request item
+	var chg = new GlideRecord('change_request');
+	chg.addQuery('number', num);
+	chg.query();
+
+	if(chg.next()){
+		return chg.sys_id;
+	}
+		return '';
 }
