da82e1f07b8a824fb8fa660d4a9c7ebf13b60fbc
Dan Covic <dan.covic@outlook.com>
2021-04-16 21:59:23 +0000

    added more scripts

--- servicenow-scripts.js
+++ servicenow-scripts.js
@@ -15,24 +15,49 @@ if(gr.next()) {
    gr.update();
 }
 
-// CANCEL CHANGE TASKS OF A CHANGE REQUEST
-var change_arr = ['CHG0055651', 'CHG0057994', 'CHG0059665','CHG0060561']; //Iterate through the changes that need their change tasks to be cancelled
-
-for (var i=0; i < change_arr.length; i++){
-	
-	// UPDATE CHANGE TASK
+// UPDATE CHANGE TASK EX2
+if(current.number == 4){
 	var gr = new GlideRecord('change_task');
-	gr.addQuery('number', 'CTASK0111702');
+	gr.addQuery('change_request.number', current.number); // g_form.GetValue only works on client scripts and br's run on the server.. so you can't use g_form, instead use current.number
 	gr.query();
-	if(gr.next()) {
+	while(gr.next()) {
            gr.state = '4';
            gr.update();
 	}
 }
 
 
-// CREATE NEW CHANGE TASK
-var change_arr = ['CHG0040003', 'CHG0040002']; //Iterate through the changes that need these new change tasks
+// CREATE CHANGE TASKS FOR A SINGLE CHANGE REQUEST
+var gr = new GlideRecord('change_task');
+// Create first change_task for each of the changes listed in the above array
+gr.addQuery('change_request', '1c87925347c12200e0ef563dbb9a7177');
+gr.query();
+gr.order = 100;
+gr.short_description = 'Notify Service Desk';
+gr.assignment_group = 'Change Management';
+gr.insert();
+
+// Create 2nd change_task for each of the changes listed in the above array
+var gr = new GlideRecord('change_task');
+gr.addQuery('change_request', '1c87925347c12200e0ef563dbb9a7177');
+gr.query();
+gr.order = 200;
+gr.short_description = 'Implement the change';
+gr.assignment_group = 'Change Management';
+gr.insert();
+
+// Create 3nd change_task for each of the changes listed in the above array
+var gr = new GlideRecord('change_task');
+gr.addQuery('change_request', '1c87925347c12200e0ef563dbb9a7177');
+gr.query();
+gr.order = 300;
+gr.short_description = 'Validate the change';
+gr.assignment_group = 'Change Management';
+gr.insert();
+
+
+// CREATE CHANGE TASKS FOR MULTIPLE CHANGE REQUESTS
+var change_arr = ['CHG0040005', 'CHG0040006']; //Iterate through the changes that need these new change tasks
 
 for (var i=0; i < change_arr.length; i++){
 	// Create first change_task for each of the changes listed in the above array
@@ -72,3 +97,45 @@ function getSysid(num){
 	}
 		return '';
 }
+
+// CANCEL CHANGE TASKS FOR A SINGLE CHANGE REQUEST
+var grt = new GlideRecord('change_task');
+grt.addQuery('change_request', 'eaf5d21347c12200e0ef563dbb9a7109');
+grt.query();
+while(grt.next()) {
+	if (grt.state != 4) {
+		grt.state = 4;
+		grt.update();
+	}
+}
+
+// CANCEL CHANGE TASKS FOR MULTIPLE CHANGE REQUESTS
+var change_arr = ['CHG0040003', 'CHG0040005', 'CHG0040006']; //Iterate through the changes that need their change tasks to be cancelled
+
+for (var i=0; i < change_arr.length; i++){
+	
+	// UPDATE CHANGE TASK
+	var gr = new GlideRecord('change_task');
+	gr.change_request = getSysid(change_arr[i]);
+	while(grt.next()) {
+		if (grt.state != 4) {
+			grt.state = 4;
+			grt.update();
+	}
+}
+
+function getSysid(num){
+
+	// get the sys_id for a change request item
+	var chg = new GlideRecord('change_request');
+	chg.addQuery('number', num);
+	chg.query();
+
+	if(chg.next()){
+		return chg.sys_id;
+	}
+		return '';
+}
+
+
+
