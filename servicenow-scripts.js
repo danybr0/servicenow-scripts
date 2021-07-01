75e43c719ae0ec03c97a6e85f0a05441a40a0da3
Adrian <75500003+bzul85@users.noreply.github.com>
2021-07-01 08:42:30 +0200

    Update servicenow-scripts.js

--- servicenow-scripts.js
+++ servicenow-scripts.js
@@ -16,6 +16,27 @@ if(gr.next()) {
 }
 
 
+//  Change Closure Code in Change Request
+var gr = new GlideRecord('change_request');
+gr.addQuery('number', 'CHG0069215');
+gr.query();
+
+while(gr.next()){
+gr.u_qs_closure_code = '5';
+gr.update();
+} 
+
+
+// CHANGE STATE IN CHANGE REQUEST 
+var gr = new GlideRecord('change_request');
+gr.addQuery('number', 'CHG0042583');
+gr.query();
+if(gr.next()) {
+   gr.state = '308';          //308 CANCELLED
+   gr.update();
+}
+
+
 // UPDATE CHANGE TASK
 var gr = new GlideRecord('change_task');
 gr.addQuery('number', 'CTASK0111702');
